0eb468d8cbc91e44da5f66567125e49bc7ed1c5c
Loay Ahmed <loayahmed304@gmail.com>
2024-08-07 18:50:18 +0300

    :window: Implemented add-recipe modal window

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,6 +7,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 const recipeContainer = document.querySelector('.recipe');
 
---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,33 @@
+import View from './view.js';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnUpload = document.querySelector('.upload__btn');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
+
+  toggleWindow() {
+    this._window.classList.toggle('hidden');
+    this._overlay.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+}
+
+export default new AddRecipeView();
